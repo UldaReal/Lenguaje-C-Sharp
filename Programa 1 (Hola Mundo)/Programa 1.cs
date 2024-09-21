77360bb7eb57a5b0699000dcb30b99591f868645
UldaReal <ulisesdacel1704@gmail.com>
2024-09-21 12:59:32 -0600

    Programa 1: Hola Mundo

---
+++ Programa 1 (Hola Mundo)/Programa 1.cs
@@ -0,0 +1,11 @@
+﻿using System;//Importacion de la biblioteca System
+namespace Programa1 //Nombre general
+{
+    class Programa //Nombre de la calse
+    {
+        static void Main(string[] args)//Metodo principal
+        {
+            Console.WriteLine("Hola Mundo");//Llamar a la consola a imprimir: Hola Mundo
+        }
+    }
+}
